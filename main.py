98343094228b656b1c11d6266100a4843bc66842
ayaatwih <atwaaya20@gmail.com>
2022-09-19 22:19:40 +0300

    third question

--- main.py
+++ main.py
@@ -1,67 +1,17 @@
-import math
+list1 = []
 
+for x in range(5):
+    number1= input("enter the number:\n")
+    list1.append(float(number1))
 
-number1 = input("Add the first number:")
+min = list1[0]
+max = 0
 
-if number1.isalpha() or number1.isspace():
-    print("Enter true value!")
+for item in list1:
+    if item < min:
+        min = item
+    elif item > max :
+        max = item
 
-print("____________________")
-print("1. + \n2.- \n3.* \n4./ \n5.^ \n6.% ")
-
-operation = input(" inter the opertion you need :")
-
-number2 = input("Add the second number:")
-
-if operation == "1" or operation == "+":
-    result1 = float(number1) + float(number2)
-    print(f"the result is {result1}")
-
-elif operation == "2" or operation == "-":
-    result1 = float(number1) - float(number2)
-    print(f"the result is {result1}")
-
-elif operation == "3" or operation == "*":
-    result1 = float(number1) * float(number2)
-    print(f"the result is {result1}")
-
-elif operation == "4" or operation == "/":
-    result1 = float(number1) / float(number2)
-    print(f"the result is {result1}")
-
-elif operation == "5" or operation == "^":
-    result1 = float(number1) ^ float(number2)
-    print(f"the result is {result1}")
-
-
-elif operation == "6" or operation == "%":
-    result1 = float(number1) % float(number2)
-    print(f"the result is {result1}")
-
-print("_________________")
-operation2 = input("1.round \n2.floor \n3.ceil \n4.Integer \n5.exit \n.Enter the operation for result:")
-
-if operation2 == "1":
-    result = round(result1)
-    print(f"the final result is {result}")
-
-elif operation2 == "2":
-    result = math.floor(result1)
-    print(f"the final result is {result}")
-
-elif operation2 == "3":
-    result = math.ceil(result1)
-    print(f"the final result is {result}")
-
-elif operation2 == "4":
-    result = int(result1)
-    print(f"the final result is {result}")
-
-
-elif operation2 == "5":
-    print(f"the final result is {result1}")
-    exit()
-
-
-
-
+print(max)
+print(min)
